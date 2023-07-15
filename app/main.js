cdbd271f8969f7c6d56c262a3b2f9244a4a59743
Miroslav <bekyaroff@gmail.com>
2023-07-15 14:38:56 +0300

    next stage

--- app/main.js
+++ app/main.js
@@ -3,16 +3,18 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
+// Uncomment this block to pass the first stage  test
 const server = net.createServer((connection) => {
-  // Handle connection 123 
-  connection.on('data', () => {
-    console.log('connected');
-    connection.write('+PONG\r\n');
-  })
-  connection.on('end', () => {
-    console.log("you've disconnected")
-  })
+  // Handle connection 123
+  connection.on("data", () => {
+    console.log("connected");
+    connection.write("+PONG\r\n");
+    connection.write("+PONG\r\n");
+    connection.write("+PONG\r\n");
+  });
+  connection.on("end", () => {
+    console.log("you've disconnected");
+  });
 });
 
 server.listen(6379, "127.0.0.1");
